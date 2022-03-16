d18c95d94dfa1f258dbffe1e57947bce2c6f4b52
YauheniMel <207melnik@gmail.com>
2022-03-16 15:13:01 +0300

    refactor: Update style auth page

--- src/components/AuthPage/AuthPage.jsx
+++ src/components/AuthPage/AuthPage.jsx
@@ -12,37 +12,48 @@ export default function AuthPage({
   const refPassword = React.createRef();
 
   return (
-    <div className={style.wrapper}>
-      <h2>Welcome! Please Log In!</h2>
+    <div className={`${style.container} container`}>
+      <div className={style.wrapper}>
+        <h2>Welcome! Please Log In!</h2>
+      </div>
       <form onSubmit={onSubmit} action="">
-        <legend>
-          Login form
-          <input
-            className={style.login}
-            value={state.loginValue}
-            type="text"
-            onChange={() => onChangeLogin(refLogin)}
-            ref={refLogin}
-            required
-          />
-          <input
-            className={style.password}
-            value={state.passwordValue}
-            type="password"
-            onChange={() => onChangePassword(refPassword)}
-            ref={refPassword}
-            required
-          />
-          <button className={style.button_submit} type="submit">
-            Authorization
-          </button>
-          <button className={style.button_restore}>
-            Forget your password?
-          </button>
-          <Link path="/signup" className={style.button_restore}>
-            SignUp
-          </Link>
-        </legend>
+        <fieldset>
+          <legend>
+            Login form
+          </legend>
+            <input
+              className={style.login}
+              value={state.loginValue}
+              type="text"
+              onChange={() => onChangeLogin(refLogin)}
+              ref={refLogin}
+              required
+            />
+            <input
+              className={style.password}
+              value={state.passwordValue}
+              type="password"
+              onChange={() => onChangePassword(refPassword)}
+              ref={refPassword}
+              required
+            />
+            <div className={style.action}>
+              <div className={style.buttons}>
+                <button className={style.button_submit} type="submit">
+                  Authorization
+                </button>
+                <button
+                  className={style.button_restore}
+                  type="button"
+                >
+                  Forget your password?
+                </button>
+              </div>
+              <Link path="/signup" className={style.link}>
+                SignUp
+              </Link>
+            </div>
+        </fieldset>
       </form>
     </div>
   );
